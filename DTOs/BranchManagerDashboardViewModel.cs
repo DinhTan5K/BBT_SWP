391f23dfd3b7bafa4f28fb301ed124daba3b86c5
DinhTan5K <tanv3660@gmail.com>
2025-10-27 20:17:18 +0700

    delete bmanager

--- DTOs/BranchManagerDashboardViewModel.cs
+++
@@ -1,8 +0,0 @@
-public class BranchManagerDashboardViewModel
-{
-    public string ManagerName { get; set; } = string.Empty;
-    public string BranchName { get; set; } = string.Empty;
-    public int EmployeeCount { get; set; }
-    public int TodayOrdersCount { get; set; }
-    public decimal TodayRevenue { get; set; }
-}
--- DTOs/SalaryReport.cs
+++
@@ -1,9 +0,0 @@
-public class SalaryReport
-{
-    public string? EmployeeID { get; set; }
-    public string? FullName { get; set; }
-
-    public int TotalShifts { get; set; }
-    public double TotalHours { get; set; }
-    public double TotalSalary { get; set; }
-}
